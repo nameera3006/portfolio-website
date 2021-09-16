a212913c61afcbd4bd164c9e81ab66ae2c385e8f
Nameera Shaikh <nameerasshaikh@gmail.com>
2021-09-16 15:58:50 +0530

    add all my files

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector('#menu');
+const navMenu = document.querySelector('nav');
+
+
+menuButton.addEventListener('click', function () {
+    if (navMenu.classList.contains('d-flex')) {
+        navMenu.classList.add('d-flex');
+        navMenu.classList.remove('d-none');
+    } else {
+        navMenu.classList.remove('d-flex');
+        navMenu.classList.add('d-none');
+    }
+});
